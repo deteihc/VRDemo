218c4a5adbb538eb7bf8a55e091d6e7f57a2585a
deteihc <deteihc88@qq.com>
2017-02-22 17:27:51 +0800

    添加输入系统样例

---
+++ Assets/_Examples/Scripts/VRInput/VRInputDefined.cs
@@ -0,0 +1,48 @@
+﻿namespace Examples
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public static class VRInputDefined
+    {
+        public static VRControllerEventArgs ChangeArgsType(Hand hand, VRTK.ControllerInteractionEventArgs e)
+        {
+            VRControllerEventArgs ee;
+            ee.hand = hand;
+            ee.controllerIndex = e.controllerIndex;
+            ee.buttonPressure = e.buttonPressure;
+            ee.touchpadAngle = e.touchpadAngle;
+            ee.touchpadAxis = e.touchpadAxis;
+            return ee;
+        }
+
+        public static VRControllerEventArgs MakeEventArgs(Hand hand, Vector2 touchpadAxis, float buttonPressure = 0, float touchpadAngle = 0, uint controllerIndex = 0)
+        {
+            VRControllerEventArgs e;
+            e.hand = hand;
+            e.buttonPressure = buttonPressure;
+            e.controllerIndex = controllerIndex;
+            e.touchpadAngle = touchpadAngle;
+            e.touchpadAxis = touchpadAxis;
+            return e;
+        }
+    }
+
+    public enum Hand { NULL, LEFT, RIGHT }
+
+    /// <summary>
+    /// Event Payload
+    /// </summary>
+    /// <param name="controllerIndex">The index of the controller that was used.</param>
+    /// <param name="buttonPressure">The amount of pressure being applied to the button pressed. `0f` to `1f`.</param>
+    /// <param name="touchpadAxis">The position the touchpad is touched at. `(0,0)` to `(1,1)`.</param>
+    /// <param name="touchpadAngle">The rotational position the touchpad is being touched at, 0 being top, 180 being bottom and all other angles accordingly. `0f` to `360f`.</param>
+    public struct VRControllerEventArgs
+    {
+        public Hand hand;
+        public uint controllerIndex;
+        public float buttonPressure;
+        public Vector2 touchpadAxis;
+        public float touchpadAngle;
+    }
+}
---
+++ Assets/_Examples/Scripts/VRInput/VRInputManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Examples
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public class VRInputManager : MonoBehaviour
+    {
+        #region SINGLETON
+
+        private static VRInputManager instance = null;
+
+        public static VRInputManager Instance
+        {
+            get
+            {
+                if (instance) return instance;
+                else
+                {
+                    Debug.LogError("VRInputManager is uninitialized!");
+                    return null;
+                }
+            }
+        }
+
+        #endregion SINGLETON
+
+        #region Init
+
+        private void Awake()
+        {
+            instance = this;
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+
+        #endregion Init
+    }
+}
