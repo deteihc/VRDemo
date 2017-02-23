6c62ce01cce68d73b838c9c9831811a95f622673
deteihc <deteihc88@qq.com>
2017-02-23 13:38:53 +0800

    合并模拟器与SteamVR

--- Assets/_Examples/Scripts/VRInput/VRInputManager.cs
+++ Assets/_Examples/Scripts/VRInput/VRInputManager.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections;
     using UnityEngine;
+    using VRTK;
 
     public class VRInputManager : MonoBehaviour
     {
@@ -34,8 +35,33 @@
 
         private void Init()
         {
+            InitController();
+        }
+
+        private void InitController()
+        {
+            GameObject hand = VRTK_SDKManager.instance.actualBoundaries.transform.FindChild("Hand").gameObject;
+            if (!VRInputDefined.isHMDConnected)
+            {
+                Transform tmpHead = VRTK_SDKManager.instance.actualHeadset.transform.parent;
+                VRTK_SDKManager.instance.actualHeadset.transform.SetParent(VRTK_SDKManager.instance.actualBoundaries.transform, false);
+                tmpHead.SetParent(VRTK_SDKManager.instance.actualHeadset.transform, false);
+                tmpHead.GetChild(0).SetParent(VRTK_SDKManager.instance.actualHeadset.transform, false);
+                tmpHead.gameObject.SetActive(false);
+                GameObject leftHand = Instantiate(hand, VRTK_SDKManager.instance.actualLeftController.transform, false) as GameObject;
+                leftHand.name = "Hand";
+                GameObject rightHand = Instantiate(hand, VRTK_SDKManager.instance.actualRightController.transform, false) as GameObject;
+                rightHand.name = "Hand";
+
+                VRTK_SDKManager.instance.actualLeftController.AddComponent<SDK_ControllerSim>();
+                VRTK_SDKManager.instance.actualRightController.AddComponent<SDK_ControllerSim>();
+                VRTK_SDKManager.instance.actualBoundaries.AddComponent<Examples.SDK_InputSimulator>();
+            }
+            DestroyImmediate(hand);
         }
 
         #endregion Init
+
+
     }
 }
